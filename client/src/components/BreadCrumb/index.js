a06c91d32d4e104607d09f275f4e52ae3c76c27c
Romario <romaef23@gmail.com>
2020-08-24 09:30:16 -0500

    Adding the Bread Crumb

---
+++ client/src/components/BreadCrumb/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './styles.scss';
+
+export const BreadCrumb = () => {
+
+  return (
+    <ul class="breadcrumbs">
+      <li><a href="">Electrónica, Audio y Video</a></li>
+      <li><a href="">iPod</a></li>
+      <li><a href="">Reproductores</a></li>
+      <li><a href="">iPod touch</a></li>
+      <li><a href="">32GB</a></li>
+    </ul>
+  )
+}
--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,10 +1,12 @@
 import React, { Fragment } from "react"
 import { SearchBox } from '../components/SearchBox'
+import { BreadCrumb } from '../components/BreadCrumb'
 
 export const Home = (path) => {
   return (
     <Fragment>
       <SearchBox />
+      <BreadCrumb />
     </Fragment>
   )
 }
